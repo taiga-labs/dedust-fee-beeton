094e379cfb869dd160deaab830856ac1fa7cd498
zpnst <akuratnonenado@gmail.com>
2025-02-21 21:27:04 +0300

    redy2.0

--- scripts/mintJettons.ts
+++ scripts/mintJettons.ts
@@ -6,7 +6,7 @@ import { JettonMinter } from '../wrappers/JettonMinter';
 dotenv.config();
 const JETTON_MINTER_ADDRESS = process.env.JETTON_MINTER_ADDRESS as string;
 
-const TO_MINT: bigint = 1000000n;
+const TO_MINT: bigint = 10_000_000_000n;
 
 export async function run(provider: NetworkProvider) {
 
@@ -15,7 +15,7 @@ export async function run(provider: NetworkProvider) {
     await jettonMinter.sendMint(provider.sender(), {
         value: toNano('0.08'),
         queryId: BigInt(Math.floor(Date.now() / 1000)),
-        toAddress: Address.parse("UQCVSjegGs-QZqfko9lKBHMNQt6dHPSI3m8xD6ipmtHRRv_G"),
+        toAddress: Address.parse("UQBpfaGXXiCYp1aE7DvJD0e8IVCdZ5mczwj8W2_put89e2Fz"),
         tonAmount: toNano('0.05'),
         jettonAmountToMint: TO_MINT * 10n**9n,
         fromAddress: Address.parse(JETTON_MINTER_ADDRESS)
